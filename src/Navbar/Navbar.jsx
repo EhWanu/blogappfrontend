5416a22243c65728368b6856f4c5e9a293ef4faf
EhWanu <cameronjshadgett@gmail.com>
2021-10-11 10:15:39 -0400

    added social media icons

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <div className='top'>
-            <div className="topLeft">l</div>
+            <div className="topLeft">
+                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-twitter-square"></i>
+                <i class="fab fa-pinterest-square"></i>
+                <i class="fab fa-instagram-square"></i>
+            </div>
             <div className="topCenter">c</div>
             <div className="topRight ">r</div>
         </div>
