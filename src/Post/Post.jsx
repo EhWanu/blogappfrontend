66b079d7bf48975adde846bab27bb8fb2425b7e6
EhWanu <cameronjshadgett@gmail.com>
2021-10-13 11:37:08 -0400

    Post boilderplate

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -4,7 +4,20 @@ import './Post.css'
 export default function Post() {
     return (
         <div className="post">
-            <img src="" alt="" />
+            <img 
+            className="postImg"
+                src="https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80" 
+                alt="" 
+            />
+            <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">Music</span>
+                    <span className="postCat">Life</span>
+                </div>
+                <span className="postTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                <hr/>
+                <span className="postDate">1 hour ago</span>
+            </div>
         </div>
     )
 }
