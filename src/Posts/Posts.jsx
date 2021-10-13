5e84dffd3046fc6f393bc7470187f9c9be72bdbe
EhWanu <cameronjshadgett@gmail.com>
2021-10-13 11:32:13 -0400

    created post component and embedded in it Posts

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -1,9 +1,15 @@
+import Post from '../Post/Post'
 import './Posts.css'
 
 export default function Posts() {
     return (
         <div className="posts">
-            posts
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
         </div>
     )
 }
