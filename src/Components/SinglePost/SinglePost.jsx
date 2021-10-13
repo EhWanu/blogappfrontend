55b5bb41d2b1be2d518142a42ca606d59d0dc5c1
EhWanu <cameronjshadgett@gmail.com>
2021-10-13 16:16:42 -0400

    fixed stylings and font import

--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -3,7 +3,7 @@ import './SinglePost.css'
 export default function SinglePost() {
     return (
         <div className="singlePost">
-            <div className="singPostWrapper">
+            <div className="singlePostWrapper">
                 <img 
                     src="https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80" 
                     alt="" 
@@ -17,10 +17,10 @@ export default function SinglePost() {
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author:<b>JIM FLIM</b></span>
+                    <span className="singlePostAuthor">Author: <b>JIM FLIM</b></span>
                     <span className="singlePostDate">1 Hour ago</span>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.</p>
+                <p className="singlePostDescription">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro numquam qui earum cum sapiente reprehenderit accusamus repellat sed repellendus, sit, quasi nostrum nobis iste, harum vel consectetur ipsam beatae voluptas.</p>
             </div>
         </div>
     )
