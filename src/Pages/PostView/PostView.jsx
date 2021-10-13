20c7629bfdc5769d4f09c1477b174a009df0f3a3
EhWanu <cameronjshadgett@gmail.com>
2021-10-13 14:38:34 -0400

    updated folder structure and fixed all imports

---
+++ src/Pages/PostView/PostView.jsx
@@ -0,0 +1,15 @@
+import './PostView.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import SinglePost from '../../Components/SinglePost/SinglePost'
+
+
+export default function PostView() {
+    return (
+        <div className="postView">
+            something
+            <SinglePost />
+            <Sidebar />
+            
+        </div>
+    )
+}
