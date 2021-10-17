9cb268ce1470cf9effbe67b26405a7e65660dbae
EhWanu <cameronjshadgett@gmail.com>
2021-10-16 21:50:56 -0400

    added second form

--- src/Pages/Write/Write.jsx
+++ src/Pages/Write/Write.jsx
@@ -9,7 +9,10 @@ export default function Write() {
                     <i class="fas fa-plus"></i>
                     </label>
                     <input type="file" id='fileInput' style={{display: "none"}} />
-                    <input type="text" placeholder="Title" className="writeInput" />
+                    <input type="text" placeholder="Title" className="writeInput" autofocus={true} />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea plaholder="Tell Your Story..." type="text" className="writeInput writeText"></textarea>
                 </div>
             </form>
         </div>
