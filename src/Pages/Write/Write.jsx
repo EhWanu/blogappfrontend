2d6b15133e1ce17fc2c2b179543a27408b955ca8
EhWanu <cameronjshadgett@gmail.com>
2021-10-16 21:46:40 -0400

    added plus icon

--- src/Pages/Write/Write.jsx
+++ src/Pages/Write/Write.jsx
@@ -5,6 +5,9 @@ export default function Write() {
         <div className="write">
             <form className="writeForm">
                 <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                    <i class="fas fa-plus"></i>
+                    </label>
                     <input type="file" id='fileInput' />
                     <input type="text" placeholder="Title" />
                 </div>
