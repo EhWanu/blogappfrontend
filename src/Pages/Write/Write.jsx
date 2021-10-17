1957011926214e14f404fd93043f64bfd451427a
EhWanu <cameronjshadgett@gmail.com>
2021-10-16 21:45:30 -0400

    skeleton layout

--- src/Pages/Write/Write.jsx
+++ src/Pages/Write/Write.jsx
@@ -3,7 +3,12 @@ import './Write.css';
 export default function Write() {
     return (
         <div className="write">
-            <form className="writeForm"></form>
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <input type="file" id='fileInput' />
+                    <input type="text" placeholder="Title" />
+                </div>
+            </form>
         </div>
     )
 }
