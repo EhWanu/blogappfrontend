0c44a89908928e399ddcf9a0d024d64f850bfc2b
EhWanu <cameronjshadgett@gmail.com>
2021-10-18 14:47:08 -0400

    boiler plate code

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,26 @@
-import './Login.css';
+import "./Login.css";
 
 export default function Login() {
-    return (
-        <div className="login">
-            login 
-        </div>
-    )
+	return (
+		<div className="login">
+			<form className="loginForm">
+				<label>Email</label>
+				<input
+					type="text"
+					placeholder="Enter Your Email... "
+				/>
+				<label>Password</label>
+				<input
+					type="text"
+					placeholder="Enter Your Password... "
+				/>
+				<button className="loginButton">
+					Login
+				</button>
+				<button className="loginRegisterButton">
+					Register
+				</button>
+			</form>
+		</div>
+	);
 }
