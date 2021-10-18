5a68f5a91570973a7a62923a89153ed2338b1f7a
EhWanu <cameronjshadgett@gmail.com>
2021-10-18 12:42:35 -0400

    added stylings

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -7,7 +7,7 @@ export default function Settings() {
            <div className="settingsWrapper">
                 <div className="settingsTitle">
                     <span className="settingsUpdateTitle">Update Your Account</span>
-                    <span className="settingsDelete Title">Delete Account</span>
+                    <span className="settingsDeleteTitle">Delete Account</span>
                 </div>  
                 <form className="settingsForm">
                     <label>Profile Picture</label>
@@ -27,7 +27,7 @@ export default function Settings() {
                     <input type="text" placeholder='jim@gmail.com' />
                     <label>Password</label>
                     <input type="password"/>
-                    <button className="settingsSUBMIT">Update</button>
+                    <button className="settingsSubmit">Update</button>
                     
                 </form> 
             </div> 
