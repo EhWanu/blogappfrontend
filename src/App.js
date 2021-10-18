23aa2ebe6b4012602d7f410c3f6d777904d1ecde
EhWanu <cameronjshadgett@gmail.com>
2021-10-18 12:04:26 -0400

    created functional component and import sidebar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import PostView from './Pages/PostView/PostView'
+import Settings from "./Pages/Settings/Settings";
 import Write from "./Pages/Write/Write";
 
 function App() {
   return (
     <>
     <Navbar />
-    <Write />
+    <Settings />
     </>
   );
 }
---
+++ src/Pages/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import './Settings.css'
+
+export default function Settings() {
+    return (
+        <div className="settings">
+           <div className="settingWrapper"></div> 
+           <Sidebar />
+        </div>
+    )
+}
