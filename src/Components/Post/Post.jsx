f3e2ffa1f44fe66a455b6dd6e9ca7975f4d85892
EhWanu <cameronjshadgett@gmail.com>
2022-01-11 13:12:32 -0500

    fetch and display post/data

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,29 +1,50 @@
+import "./Post.css";
 
-import './Post.css'
-
-export default function Post() {
-    return (
-        <div className="post">
-            <img 
-            className="postImg"
-                src="https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80" 
-                alt="" 
-            />
-            <div className="postInfo">
-                <div className="postCats">
-                    <span className="postCat">Music</span>
-                    <span className="postCat">Life</span>
-                </div>
-                <span className="postTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
-                <hr/>
-                <span className="postDate">1 hour ago</span>
-            </div>
-            <p className="postDescription">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias debitis alias nesciunt. Magnam numquam, ducimus repellat vitae et aliquam dolor quae velit, beatae molestias veniam nulla? Deleniti, officia inventore.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias debitis alias nesciunt. Magnam numquam, ducimus repellat vitae et aliquam dolor quae velit, beatae molestias veniam nulla? Deleniti, officia inventore.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias debitis alias nesciunt. Magnam numquam, ducimus repellat vitae et aliquam dolor quae velit, beatae molestias veniam nulla? Deleniti, officia inventore.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias debitis alias nesciunt. Magnam numquam, ducimus repellat vitae et aliquam dolor quae velit, beatae molestias veniam nulla? Deleniti, officia inventore.
-            </p>
-        </div>
-    )
+export default function Post({ post }) {
+	return (
+		<div className="post">
+			<img
+				className="postImg"
+				src="https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80"
+				alt=""
+			/>
+			<div className="postInfo">
+				<div className="postCats">
+					<span className="postCat">Music</span>
+					<span className="postCat">Life</span>
+				</div>
+				<span className="postTitle">
+					{post.title}
+				</span>
+				<hr />
+				<span className="postDate">
+					{post.createdAt}
+				</span>
+			</div>
+			<p className="postDescription">
+				Lorem ipsum dolor sit amet consectetur
+				adipisicing elit. Voluptates molestias debitis
+				alias nesciunt. Magnam numquam, ducimus
+				repellat vitae et aliquam dolor quae velit,
+				beatae molestias veniam nulla? Deleniti,
+				officia inventore. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Voluptates
+				molestias debitis alias nesciunt. Magnam
+				numquam, ducimus repellat vitae et aliquam
+				dolor quae velit, beatae molestias veniam
+				nulla? Deleniti, officia inventore. Lorem
+				ipsum dolor sit amet consectetur adipisicing
+				elit. Voluptates molestias debitis alias
+				nesciunt. Magnam numquam, ducimus repellat
+				vitae et aliquam dolor quae velit, beatae
+				molestias veniam nulla? Deleniti, officia
+				inventore. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Voluptates
+				molestias debitis alias nesciunt. Magnam
+				numquam, ducimus repellat vitae et aliquam
+				dolor quae velit, beatae molestias veniam
+				nulla? Deleniti, officia inventore.
+			</p>
+		</div>
+	);
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,27 @@
-import Header from '../../Components/Header/Header'
-import Posts from '../../Components/Posts/Posts'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import './Home.css'
+import { useEffect, useState } from "react";
+import Header from "../../Components/Header/Header";
+import Posts from "../../Components/Posts/Posts";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./Home.css";
+import axios from "axios";
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-                <div className="home">
-                    <Posts />
-                    <Sidebar />
-                </div>
-        </>
-    )
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const res = await axios.get("/posts");
+			setPosts(res.data);
+		};
+		fetchPosts();
+	}, []);
+	return (
+		<>
+			<Header />
+			<div className="home">
+				<Posts posts={posts} />
+				<Sidebar />
+			</div>
+		</>
+	);
 }
