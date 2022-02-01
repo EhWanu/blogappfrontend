35e6b516659a2be253d5592f7cd5c75c5d5ccb19
EhWanu <cameronjshadgett@gmail.com>
2022-01-31 21:46:37 -0500

    added delete functionality, only author can delete their posts

--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -1,14 +1,20 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import {
+	useContext,
+	useEffect,
+	useState,
+} from "react";
 import { useLocation } from "react-router";
 import "./SinglePost.css";
 import { Link } from "react-router-dom";
+import { Context } from "../../Context/Context";
 
 export default function SinglePost() {
 	const location = useLocation();
 	const path = location.pathname.split("/")[2];
 	const [post, setPost] = useState({});
 	const PF = "http://localhost:5000/images/";
+	const { user } = useContext(Context);
 
 	useEffect(() => {
 		const getPost = async () => {
@@ -17,6 +23,16 @@ export default function SinglePost() {
 		};
 		getPost();
 	}, [path]);
+
+	const handleDelete = async () => {
+		try {
+			await axios.delete(`/posts/${post._id}`, {
+				data: { username: user.username },
+			});
+			//HOMEPAGE REDIRECT
+			window.location.replace("/");
+		} catch (err) {}
+	};
 	return (
 		<div className="singlePost">
 			<div className="singlePostWrapper">
@@ -29,10 +45,15 @@ export default function SinglePost() {
 				)}
 				<h1 className="singlePostTitle">
 					{post.title}
-					<div className="singlePostEdit">
-						<i className="singlePostIcon far fa-edit"></i>
-						<i className="singlePostIcon far fa-trash-alt"></i>
-					</div>
+					{post.username === user?.username && (
+						<div className="singlePostEdit">
+							<i className="singlePostIcon far fa-edit"></i>
+							<i
+								className="singlePostIcon far fa-trash-alt"
+								onClick={handleDelete}
+							></i>
+						</div>
+					)}
 				</h1>
 				<div className="singlePostInfo">
 					<span className="singlePostAuthor">
